4ebf8648b7852c49bc4fc09a609cfc054325e6ab
WASCIK-appDevelopment <lewismike0435@gmail.com>
2026-08-09 12:17:22 -0500

    Fix first RevoMatic product image batch

--- app/affiliate-services/revo-image-urls.ts
+++ app/affiliate-services/revo-image-urls.ts
@@ -5,9 +5,15 @@ export const suppliedProductImageUrls: Record<number, string[]> = {
     "https://us.homeaccess.philips/cdn/shop/files/M2_4797baea-362a-45cb-9cc2-4ca261a720ab.jpg?v=1780906824&width=1200",
     "https://us.homeaccess.philips/cdn/shop/files/M4_b1988f81-60fb-4395-a0ad-71c258cc7050.jpg?v=1780906822&width=1200"
   ],
-  11: ["https://revomadic.com/cdn/shop/files/REVO-Therapy-Massage-Oil.jpg"],
-  13: ["https://revomadic.com/cdn/shop/files/REVO-Face-Genie-Pink.jpg"],
-  14: ["https://revomadic.com/cdn/shop/files/REVO-Soothing-Cream.jpg"],
+  11: [
+    "https://ucarecdn.com/78b4188d-a2c2-4ecd-8881-22c50af0ffec/-/format/auto/-/preview/3000x3000/-/quality/lighter/Therapy.png"
+  ],
+  13: [
+    "https://ucarecdn.com/59dcd189-a045-4f8a-b8f1-7015ff866926/-/format/auto/-/preview/3000x3000/-/quality/lighter/Face-Genie-images.png"
+  ],
+  14: [
+    "https://m.media-amazon.com/images/I/619dv2KmAGL._AC_SL1500_.jpg"
+  ],
   15: ["https://revomadic.com/cdn/shop/files/REVO-Sculptor.jpg"],
   16: ["https://revomadic.com/cdn/shop/files/REVO-Collagen-Jelly.jpg"],
   19: ["https://revomadic.com/cdn/shop/files/REVO-Face-Genie-Collagen-Jelly-Bundle.jpg"]

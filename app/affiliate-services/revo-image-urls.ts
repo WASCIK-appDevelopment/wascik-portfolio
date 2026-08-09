71a328b24678dbed854c49e35e12188447890877
WASCIK-appDevelopment <lewismike0435@gmail.com>
2026-08-09 12:26:56 -0500

    Remove duplicate Philips 3000 gallery image

--- app/affiliate-services/revo-image-urls.ts
+++ app/affiliate-services/revo-image-urls.ts
@@ -1,7 +1,6 @@
 export const suppliedProductImageUrls: Record<number, string[]> = {
   10: [
     "https://us.homeaccess.philips/cdn/shop/files/5dc7a49828f75ea296e0bccb4e732b99_f7c27400-acbf-472c-9057-5aed858d3d63.jpg?v=1783665920&width=1200",
-    "https://us.homeaccess.philips/cdn/shop/files/M1_339d6669-11d9-43c4-91c0-28e2519eebaf.jpg?v=1780906823&width=1200",
     "https://us.homeaccess.philips/cdn/shop/files/M2_4797baea-362a-45cb-9cc2-4ca261a720ab.jpg?v=1780906824&width=1200",
     "https://us.homeaccess.philips/cdn/shop/files/M4_b1988f81-60fb-4395-a0ad-71c258cc7050.jpg?v=1780906822&width=1200"
   ],

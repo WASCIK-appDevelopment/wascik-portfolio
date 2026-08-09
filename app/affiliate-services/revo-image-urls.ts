8478d7903f718813fd30b253b497848986cc04d6
WASCIK-appDevelopment <lewismike0435@gmail.com>
2026-08-09 11:21:02 -0500

    Stage product media URLs for remaining cards

---
+++ app/affiliate-services/revo-image-urls.ts
@@ -0,0 +1,14 @@
+export const suppliedProductImageUrls: Record<number, string[]> = {
+  10: [
+    "https://images.philips.com/is/image/philipsconsumer/DDL230X15KTW_37?$pnglarge$&wid=960",
+    "https://us.homeaccess.philips/cdn/shop/files/Voice_Assistant_Compatibility.png",
+    "https://us.homeaccess.philips/cdn/shop/files/Fingerprint_Keypad.png",
+    "https://us.homeaccess.philips/cdn/shop/files/APP_Remote_Control.png"
+  ],
+  11: ["https://revomadic.com/cdn/shop/files/REVO-Therapy-Massage-Oil.jpg"],
+  13: ["https://revomadic.com/cdn/shop/files/REVO-Face-Genie-Pink.jpg"],
+  14: ["https://revomadic.com/cdn/shop/files/REVO-Soothing-Cream.jpg"],
+  15: ["https://revomadic.com/cdn/shop/files/REVO-Sculptor.jpg"],
+  16: ["https://revomadic.com/cdn/shop/files/REVO-Collagen-Jelly.jpg"],
+  19: ["https://revomadic.com/cdn/shop/files/REVO-Face-Genie-Collagen-Jelly-Bundle.jpg"]
+};

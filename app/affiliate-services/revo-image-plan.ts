9fc19bc0824cdd9db2af071f4fdc5c45bda9f5e1
WASCIK-appDevelopment <lewismike0435@gmail.com>
2026-08-09 11:18:45 -0500

    Map Philips and RevoMatic product galleries

---
+++ app/affiliate-services/revo-image-plan.ts
@@ -0,0 +1,9 @@
+export const localProductImages: Record<number, string[]> = {
+  10: ["/affiliate/philips/retrofit-lock-main.jpg", "/affiliate/philips/retrofit-lock-voice-assistants.jpg", "/affiliate/philips/retrofit-lock-keypad.jpg", "/affiliate/philips/retrofit-lock-app.jpg"],
+  11: ["/affiliate/revomatic/therapy-massage-oil.jpg"],
+  13: ["/affiliate/revomatic/face-genie.jpg"],
+  14: ["/affiliate/revomatic/arnica-cream.jpg"],
+  15: ["/affiliate/revomatic/sculptor.jpg"],
+  16: ["/affiliate/revomatic/collagen-jelly.jpg"],
+  19: ["/affiliate/revomatic/face-genie-collagen-jelly-bundle.jpg"],
+};

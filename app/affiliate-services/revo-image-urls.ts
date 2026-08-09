b998bba0a7c5f7057f124ed2c8bb9f060efd1130
WASCIK-appDevelopment <lewismike0435@gmail.com>
2026-08-09 12:13:43 -0500

    Fix Philips 3000 product image gallery

--- app/affiliate-services/revo-image-urls.ts
+++ app/affiliate-services/revo-image-urls.ts
@@ -1,9 +1,9 @@
 export const suppliedProductImageUrls: Record<number, string[]> = {
   10: [
-    "https://images.philips.com/is/image/philipsconsumer/DDL230X15KTW_37?$pnglarge$&wid=960",
-    "https://us.homeaccess.philips/cdn/shop/files/Voice_Assistant_Compatibility.png",
-    "https://us.homeaccess.philips/cdn/shop/files/Fingerprint_Keypad.png",
-    "https://us.homeaccess.philips/cdn/shop/files/APP_Remote_Control.png"
+    "https://us.homeaccess.philips/cdn/shop/files/5dc7a49828f75ea296e0bccb4e732b99_f7c27400-acbf-472c-9057-5aed858d3d63.jpg?v=1783665920&width=1200",
+    "https://us.homeaccess.philips/cdn/shop/files/M1_339d6669-11d9-43c4-91c0-28e2519eebaf.jpg?v=1780906823&width=1200",
+    "https://us.homeaccess.philips/cdn/shop/files/M2_4797baea-362a-45cb-9cc2-4ca261a720ab.jpg?v=1780906824&width=1200",
+    "https://us.homeaccess.philips/cdn/shop/files/M4_b1988f81-60fb-4395-a0ad-71c258cc7050.jpg?v=1780906822&width=1200"
   ],
   11: ["https://revomadic.com/cdn/shop/files/REVO-Therapy-Massage-Oil.jpg"],
   13: ["https://revomadic.com/cdn/shop/files/REVO-Face-Genie-Pink.jpg"],
